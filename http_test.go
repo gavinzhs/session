76156a81f8c7fb00f31713c0ae7593db64296715
zhengsai <gavinzhs@gmail.com>
2015-10-15 13:57:55 +0800

    rm wrong file

--- http_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"testing"
-)
-
-func TestHttp(t *testing.T) {
-
-}
--- restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "remote restart"
-ulimit -n 30000
-fuser -k gotest
-GOMAXPROCS=4 GOGC=50 nohup ./gotest &
